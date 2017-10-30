589f66bf8e1c3e4ef0ec7c6bd4fe8bfaacb5cbeb
Roshan Shetty <Inquisitive-Geek@users.noreply.github.com>
2017-10-29 18:45:26 -0700

    Integrating Jareds cleaning functions in Python notebook

--- project_1948.py
+++ project_1948.py
@@ -1,5 +1,5 @@
 import re
-import string
+# import string
 from docx import Document
 
 #Prepare function to read in documents
@@ -41,7 +41,7 @@ def remove_punctuation_characters(input_text):
     #upper (optional) is True if the words are to be upper case
     #lower (optional) is True if the words are to be lower case
     #title (optional) is True if the words are to have 'title' case
-def normalize_text(input_text,upper=False,lower=False,title=False):
+def normalize_text(input_text,upper=False,lower=False,title=False,sep=" "):
     words = input_text.split()
     output_text=""
     if upper:
@@ -53,7 +53,7 @@ def normalize_text(input_text,upper=False,lower=False,title=False):
     if title:
         for word in words:
             word.title()
-    output_text=string.join(words)
+    output_text=sep.join(words)
     return output_text
         
 #function to separate out the statements of the interview.
@@ -108,3 +108,18 @@ def word_context(input_text, word_list, radius):
     else:
         print("ERROR IN TYPE")
     return output
+
+#function to clean the data
+    #text_arr is the input array of strings -- each string is a transcript of the interview
+    #cleaned_text_arr is the outputs array of cleaned strings
+def clean_text(text_arr):
+    cleaned_text_arr = []
+    for text in text_arr:
+        # Remove grouped text like (laughing) etc.
+        text = remove_grouped_text(text)
+        # Remove punctuation characters and replace P: & I: with INTERVIEWER & INTERVIEWEE
+        text = remove_punctuation_characters(text)
+        # Normalize the input text
+        text = normalize_text(text)
+        cleaned_text_arr.append(text)
+    return cleaned_text_arr
